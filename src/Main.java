a04c42e46491c61858a26ccf0b6c5f5a9cf2cdf3
unknown <duongducchinh160@gmail.com>
2022-08-08 15:07:50 +0700

    so lan xuat hien dung MAP

---
+++ src/Main.java
@@ -0,0 +1,25 @@
+import java.util.Locale;
+import java.util.Map;
+import java.util.Scanner;
+import java.util.TreeMap;
+
+public class Main {
+    public static void main(String[] args) {
+        Map<String,Integer> treeMap = new TreeMap<>();
+        Scanner sc = new Scanner(System.in);
+        System.out.println("nhap chuoi");
+        String input = sc.nextLine();
+        String [] arr = input.toLowerCase().split(" ");
+
+        for (int i = 0; i < arr.length; i++) {
+            if (treeMap.containsKey(arr[i])){
+                int value = treeMap.get(arr[i]);
+                treeMap.remove(arr[i]);
+                treeMap.put(arr[i],value +1);
+            }else {
+                treeMap.put(arr[i],1);
+            }
+        }
+        System.out.println("so lan xuat hien cua cac tu la: "+ treeMap);
+    }
+}
